dbfde795bfc98030e77eec2db6c827781bd90181
m276sing <55206638+m276sing@users.noreply.github.com>
2019-11-28 01:42:11 -0500

    Update b.cpp

--- b.cpp
+++ b.cpp
@@ -1,1 +1,15 @@
+#include <string>
+#include <iostream>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <signal.h>
+#include <csignal>
+#include <vector>
 
+int main(){
+  std::string s;
+  std::getline(std::cin, s);
+  std::cout << s;
+  std::cout << "b is ececuting/n";
+  return 0;
+}
