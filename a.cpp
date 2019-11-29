3937d7e2f4d43bcd26dda9281b08b1ddbf4d593e
m276sing <55206638+m276sing@users.noreply.github.com>
2019-11-28 20:35:26 -0500

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -7,10 +7,12 @@
 #include <vector>
 
 int main(){
+  while(true){
   std::string s;
   std::getline(std::cin, s);
   std::cout << s << " " << "a is executing\n";
   fflush(stdout);
   sleep(2);
+  }
   return 0;
 }
