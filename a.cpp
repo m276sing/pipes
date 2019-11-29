5e414201371a972234be510f4f0a89377baf9b26
m276sing <55206638+m276sing@users.noreply.github.com>
2019-11-28 19:52:14 -0500

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -3,5 +3,8 @@
 int main(){
   std::string s;
   std::getline(std::cin, s);
-  std::cout << s << "a is executing\n";
+  std::cout << s << " " << "a is executing\n";
+  fflush(stdout);
+  sleep(wait_period_new);
+  return 0;
 }
